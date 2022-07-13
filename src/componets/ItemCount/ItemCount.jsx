aa08653b4350df0be66da176311fbc053dd489a0
MatiasEGonzalez <matiasgonzalez110@gmail.com>
2022-07-13 12:52:57 -0300

    Entrega componentes 2

--- src/componets/ItemCount/ItemCount.jsx
+++ src/componets/ItemCount/ItemCount.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 
-const ItemCount = () => {
-    const [count, setCount] = useState(0)
+const ItemCount = ({ initial=1 , stock=10 , onAdd }) => {
+    const [count, setCount] = useState(1)
 
-    const sumar = () => setCount(count + 1)
-    const restar = () => setCount(count - 1)
+    const sumar = () => {
+      if (count < stock) {
+        setCount(count + 1)}
+      }
+      
+    const restar = () => {
+      if (count > 1) {
+        setCount(count - 1)
+      }
+     }
   return (
     <div className='card w-25 bg-transparent rounded-pill margen' >
         <button className='btn-xs btn-outline-success rounded-pill' onClick={sumar}> + </button>
--- src/componets/ItemListContainer/ItemListContainer.jsx
+++ src/componets/ItemListContainer/ItemListContainer.jsx
@@ -2,14 +2,17 @@ import ItemCount from "../ItemCount/ItemCount"
 
 
 const ItemListContainer = ({ saludar }) => {
-  return (
-    <div className="abs-center">
-      { saludar }
+    const onAdd = (cant) => {
+      console.log(`la cantidad es: ${cant}`)
+    }
+    return (
+      <div className="abs-center">
+        { saludar }
+        
+        <ItemCount  initial={1} stock={20} onAdd={onAdd}/>
+      </div>
       
-      <ItemCount />
-    </div>
-    
-  )
+    )
 }
 
 export default ItemListContainer
